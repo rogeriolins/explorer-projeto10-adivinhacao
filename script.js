8ba5ac06049af421ea871229527dc26c707d1eed
Rogerio Lins <rogerio_lins2002@yahoo.com>
2024-06-07 17:31:07 -0300

    Adjusting more code

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 // alert("Conectado...")
+const randomNumber = Math.round(Math.random() * 10)
+let xAttemps = 0
+
+function handleClick(event) {
+  event.preventDefault()
+  let valueNumber = document.querySelector("#inputNumber").value
+  xAttemps++
+  if (Number(valueNumber) == randomNumber) {
+    // Acertou
+    document.querySelector(".screen01").classList.add("hide")
+    document.querySelector(".screen02").classList.remove("hide")
+
+    document.querySelector(".screen02 h2 span").innerText = xAttemps
+  } else {
+    console.log(xAttemps, randomNumber)
+  }
+
+  function newGame() {
+    document.querySelector(".screen01").toogle('hide')
+    document.querySelector(".screen02").toogle("hide")
+  }
+
+}
